c059ce09d69d1af1ffa5ea0fe2e413c53a2a0cd5
unknown <20bcs2917@cuchd.in>
2024-07-20 17:11:36 +0530

    import update

--- routes/personroutes.js
+++ routes/personroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const person = require("./../models/person");
+const person = require("../models/Person");
 
 //commet added for testing purposes
 
